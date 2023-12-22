37bff459e9e21c72f0047c15f62546467d523541
Nitesh232 <hniteshmishra@gmail.com>
2023-12-22 12:20:56 +0530

    Created HikeOn REST Application

---
+++ HikeOn_REST_Application/src/main/java/org/hikeOn/HikeOnRestApplication.java
@@ -0,0 +1,13 @@
+package org.hikeOn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikeOnRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikeOnRestApplication.class, args);
+	}
+
+}
